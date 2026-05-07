1c41b6591a6b51cafd10a8f1b224c1299709f191
Dominique Linton <Dominiquellinton@gmail.com>
2026-05-07 12:48:56 -0400

    fixed ec2 module inputs

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,9 +1,29 @@
+# Pull latest Ubuntu AMI dynamically
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# EC2 MODULE
+# This calls our reusable EC2 template from ../../modules/ec2
 module "ec2" {
   source = "../../modules/ec2"
 
-  ami               = data.aws_ami.ubuntu.id
-  instance_type     = "t2.micro"
-  subnet_id         = module.vpc.subnet_id
-  security_group_id = module.vpc.security_group_id
-  name              = "terraform-dev-server"
+  # Operating system image
+  ami_id = data.aws_ami.ubuntu.id
+
+  # Free tier server size
+  instance_type = "t2.micro"
+
+  # Which subnet to place server into
+  subnet_id = module.vpc.subnet_id
+
+  # Name shown in AWS console
+  instance_name = "terraform-dev-server"
 }
