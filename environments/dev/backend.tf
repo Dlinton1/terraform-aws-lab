2a5f7e68e29c177789a56c83a53aeb0ce898ddfa
Dominique Linton <Dominiquellinton@gmail.com>
2026-05-12 15:42:40 -0400

    fixed wiring + structure mistake

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-dlinton1-devops"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
+    bucket = "terraform-state-dlinton1-devops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
 
     use_lockfile = true
 
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,3 @@
-# Pull latest Ubuntu AMI dynamically
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -10,39 +9,25 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# VPC MODULE
-# Creates networking infrastructure for AWS
-
 module "vpc" {
   source = "../../modules/vpc"
 
-  # Main private network range
-  vpc_cidr = "10.0.0.0/16"
-
-  # Smaller network inside VPC
+  vpc_cidr    = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
 }
 
 module "security_group" {
   source = "../../modules/security-group"
 
-  # Connect SG into VPC
   vpc_id = module.vpc.vpc_id
 }
 
 module "ec2" {
   source = "../../modules/ec2"
 
-  # Operating system image
-  ami_id = data.aws_ami.ubuntu.id
-
-  # Free tier server size
+  ami_id        = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-
-  # Which subnet to place server into
-  subnet_id = module.vpc.public_subnet_id
-
-  # Name shown in AWS console
+  subnet_id     = module.vpc.public_subnet_id
   instance_name = "terraform-dev-server"
 
   security_group_id = module.security_group.security_group_id
